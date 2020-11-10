04c0e43d0411faa834e8ee4d31717daa173026f4
Michael Olmos Trujillo <molmost@unal.edu.co>
2020-11-10 18:45:31 -0500

    adicionando footer

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 const botonIniciarSesion = document.getElementById("iniciarSesion");
 const botonRegistro = document.getElementById("botonRegistro");
+const botonCancelarRegistro = document.getElementById("botonCancelarRegistro");
+const botonCancelarsesion = document.getElementById("botonCancelarsesion");
 const formRegistro = document.getElementById("formRegistro");
+const formIniciarSesion = document.getElementById('formIniciarSesion');
+const iniciarSesionBoton = document.getElementById("iniciarSesion");
 console.log(botonIniciarSesion);
 console.log(botonRegistro);
+console.log(iniciarSesionBoton);
 
 //Eventos
 
-botonRegistro.addEventListener('click', (e)=>{
+botonRegistro.addEventListener('click', (e) => {
     e.preventDefault();
-formRegistro.style.top = "100px";
+    formRegistro.style.top = "100px";
+})
+
+botonCancelarRegistro.addEventListener('click', ()=>{
+    formRegistro.style.top = '-500px';
+} )
+botonCancelarsesion.addEventListener('click', ()=>{
+    formIniciarSesion.style.top = "-500px";
+})
+iniciarSesionBoton.addEventListener('click', (e)=>{
+    e.preventDefault();
+formIniciarSesion.style.top = "100px"
 })
